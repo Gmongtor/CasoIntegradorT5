5ab401ecd1842610087daea3dd6e37ca97ed38a0
Gmongtor <gmongtor@myuax.com>
2024-04-15 22:32:58 +0200

    commit

---
+++ Caso5/src/main/java/GestionInformacionCientífica/OrganizadorDocumentos.java
@@ -0,0 +1,27 @@
+package GestionInformacionCientífica;
+
+import java.io.*;
+import java.util.*;
+
+public class OrganizadorDocumentos {
+
+    public static void ordenarArchivo(String rutaArchivo) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(rutaArchivo));
+        ArrayList<String> lineas = new ArrayList<>();
+        String linea;
+
+        while ((linea = reader.readLine()) != null) {
+            lineas.add(linea);
+        }
+        reader.close();
+
+        Collections.sort(lineas);
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(rutaArchivo));
+        for (String lineaOrdenada : lineas) {
+            writer.write(lineaOrdenada);
+            writer.newLine();
+        }
+        writer.close();
+    }
+}
